eab5ede580009907027cfd5af8e436306ed9cbfc
Nathan <chicanathan@gmail.com>
2025-07-06 03:19:43 -0700

    add tests for app and index

--- src/index.tsx
+++ src/index.tsx
@@ -4,49 +4,13 @@ import * as React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import CssBaseline from '@mui/material/CssBaseline';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
 import './index.css';
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as Element);
 
-/**
- * Adding user authorization token to request headers
- * https://www.apollographql.com/docs/react/networking/authentication
- */
-const httpLink = createHttpLink({
-    uri: 'http://localhost:4000',
-    credentials: 'include'
-});
-const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : ''
-        }
-    };
-});
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-});
-
 root.render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ''}>
-            <ApolloProvider client={client}>
-                <LocalizationProvider dateAdapter={AdapterMoment}>
-                    <CssBaseline />
-                    <App />
-                </LocalizationProvider>
-            </ApolloProvider>
-        </GoogleOAuthProvider>
+        <App />
     </React.StrictMode>
 );
